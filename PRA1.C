2372d759e6a6786208499c6c0e66f0970157c461
PratikshaAdagale2107 <adagalepratiksha2107@gmail.com>
2026-08-31 08:44:08 +0530

    Restore student information display program
    Reintroduced the program to display student information with predefined values.

--- PRA1.C
+++ PRA1.C
@@ -1,3 +1,6 @@
+1.Program(1)- Write a program to make use of basi I/O functions using different data types.
+Solution(1)-predefined values
+
 #include <stdio.h>//Include standard input-output library for using printf()
 int main()
 {
